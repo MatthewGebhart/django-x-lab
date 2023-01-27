b8905305813ca526131c153494ad9e9fadb8ce0f
Matthew Gebhart <Matthewgebhart@gmail.com>
2023-01-26 23:27:41 -0800

    finished and functional, no styling

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import (
+    HomePageView,
+    JobPageView,
+    JobCreateView,
+    JobDeleteView,
+    JobDetailView,
+    JobUpdateView,
+)
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
-    path("about/", AboutPageView.as_view(), name="about"),
+    path("jobs/", JobPageView.as_view(), name="jobs"),
+    path("jobs/<int:pk>/", JobDetailView.as_view(), name="job_detail"),
+    path("jobs/create/", JobCreateView.as_view(), name="job_create"),
+    path("jobs/<int:pk>/update/", JobUpdateView.as_view(), name="job_update"),
+    path("jobs/<int:pk>/delete/", JobDeleteView.as_view(), name="job_delete"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,37 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, UpdateView, CreateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Job
 
 
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 
-class AboutPageView(TemplateView):
-    template_name = "pages/about.html"
+class JobPageView(ListView):
+    template_name = "pages/jobs.html"
+    model = Job
+    context_object_name = "jobs"
+
+
+class JobDetailView(DetailView):
+    template_name = 'pages/job_detail.html'
+    model = Job
+
+
+class JobUpdateView(UpdateView):
+    template_name = 'pages/job_update.html'
+    model = Job
+    fields = "__all__"
+
+
+class JobCreateView(CreateView):
+    template_name = 'pages/job_create.html'
+    model = Job
+    fields = "__all__"
+
+
+class JobDeleteView(DeleteView):
+    template_name = "pages/job_delete.html"
+    model = Job
+    success_url = reverse_lazy("jobs")
+
